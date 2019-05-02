29cdeb28a387e88f5f21b21c992bb7c8ba136ea8
Yohei Kishimoto <moromorokosi@gmail.com>
2019-05-02 13:51:19 +0900

    fix types

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,7 +59,7 @@ const index = () => (
               />
             </svg>
             <h5 style={{ fontWeight: 100 }}>
-              <a name="aboutme">Yohei Kishimoto</a>
+              <span id="aboutme">Yohei Kishimoto</span>
             </h5>
             <p style={{ fontWeight: 100 }}>Web Enginieer</p>
             <p>
@@ -107,6 +107,18 @@ const index = () => (
         text-decoration: none;
       }
     `}</style>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-89672019-1', 'auto');
+        ga('send', 'pageview');
+    `
+      }}
+    />
   </>
 );
 
